26ba165d264d846fd75e46c343becf2a4a85c318
maxim <shatunov-87@mail.ru>
2022-12-07 20:51:28 +0300

    DZ9

---
+++ 64/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+Console.WriteLine("Введите натуральное число N");
+int n = int.Parse(Console.ReadLine());
+if (n < 0)
+{
+    Console.WriteLine("Вы ввели не натуральное число");
+}
+void PrintNamber(int n)
+{
+    if (n > 0)
+    {
+        if (n > 1) Console.Write(n + ", ");
+        else Console.WriteLine(n);
+        PrintNamber(n - 1);
+    }
+}
+Console.WriteLine();
+PrintNamber(n);
+
---
+++ 66/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+int min;
+int max;
+if (m < n)
+{
+    min = m;
+    max = n;
+}
+else
+{
+    min = n;
+    max = m;
+}
+int Sum(int min, int max)
+{
+    int summa = 0;
+    if( max>= min && max>0)
+    {
+        summa = max + Sum( min,max-1);
+    }
+        
+    return summa;
+}
+
+
+Console.WriteLine("Результат: " + Sum(min, max));
